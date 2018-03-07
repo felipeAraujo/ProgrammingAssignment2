67e7048cefbc32819e0a313530f685a1993bd0e3
Felipe Teixeira <felipe.at.1989@gmail.com>
2018-03-07 04:54:49 -0300

    [Week 3] cacheSolve resolution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    get <- function() x
+    setSolved <- function(solved) s <<- solved
+    getSolved <- function() s
+    list(
+        get = get,
+        setSolved = setSolved,
+        getSolved = getSolved
+    )
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    s <- x$getSolved()
+    if (!is.null(s)) {
+        message('getting cached data')
+        return(s)
+    }
+    
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setSolved(s)
+    s
 }
